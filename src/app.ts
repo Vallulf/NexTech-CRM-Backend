cd090f4ae5c0a88c76c08c5922fc0cd5550030a9
Vallulf <waltersoares916@gmail.com>
2026-08-14 16:56:55 -0300

    Configurando Express

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, {
+  type Express,
+  type Request,
+  type Response
+} from 'express';
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
